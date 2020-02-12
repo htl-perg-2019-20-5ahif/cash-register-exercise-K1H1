e1a86e894a5e1cd11395fc1c3fbe9fb674086f8a
Katrin Haslinger <20150088@htbla-perg.onmicrosoft.com>
2020-02-12 20:38:22 +0100

    add homework

---
+++ CashRegister/CashRegister.UICore/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CashRegister.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CashRegister.UICore
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+		public partial class MainWindow : Window
+		{
+			private readonly MainWindowViewModel ViewModel;
+
+			public MainWindow()
+			{
+				InitializeComponent();
+
+				// Set the data context for data binding
+				DataContext = ViewModel = new MainWindowViewModel();
+
+				// When the view has been loaded, give the view model
+				// a chance to initialize.
+				Loaded += async (_, __) => await ViewModel.InitAsync();
+			}
+		}
+	}
+}
---
+++ CashRegister/CashRegister.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CashRegister.Shared;
+using CashRegister.WebApi;
+
+namespace CashRegister.WebApi.Controllers
+{
+
+	[ApiController]
+	[Route("api/products")]
+	public class ProductsController : ControllerBase
+	{
+		private readonly CashRegisterDataContext DataContext;
+
+		// Note the use of constructor injection to get the data context
+		public ProductsController(CashRegisterDataContext dataContext)
+		{
+			DataContext = dataContext;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<Product>> Get([FromQuery]string nameFilter = null)
+		{
+			IQueryable<Product> products = DataContext.Products;
+
+			// Apply filter if one is given
+			if (!string.IsNullOrEmpty(nameFilter))
+			{
+				products = products.Where(p => p.ProductName.Contains(nameFilter));
+			}
+
+			return await products.ToListAsync();
+		}
+	}
+}
